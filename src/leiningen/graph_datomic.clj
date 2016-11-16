46632325a51c392430087c3feaf598927f75f2af
James Conroy-Finn <git@invetica.co.uk>
2016-11-16 11:54:33 +0000

    Remove Leiningen plugin

--- src/leiningen/graph_datomic.clj
+++
@@ -1,9 +0,0 @@
-(ns leiningen.graph-datomic
-  (:require [leiningen.core.eval :refer (eval-in-project)]))
-
-(defn graph-datomic
-  "Render a nice graphviz of your Datomic schema"
-  [project uri & opts]
-  (eval-in-project project
-                   `(datomic-schema-grapher.core/graph-datomic ~uri :exit-on-close true)
-                   '(require 'datomic-schema-grapher.core)))
