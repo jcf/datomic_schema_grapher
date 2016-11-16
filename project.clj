61eff3fbe6559cc95db2f95edf406cf8310d88f7
James Conroy-Finn <git@invetica.co.uk>
2016-11-16 11:55:03 +0000

    Update project.clj
    - Deploy to my personal namespace
    - Bump version to 0.0.2-SNAPSHOT
    - Use my fork of Dorothy
    - Upgrade to Clojure 1.8.0
    - Stop evaluating code in Leiningen as we're no longer including a Leiningen
      plugin

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject datomic-schema-grapher "0.0.1"
+(defproject jcf/datomic-schema-grapher "0.0.2-SNAPSHOT"
   :description "A library and lein plugin for graphing the datomic schema."
   :url "https://github.com/felixflores/datomic_schema_grapher"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[dorothy "0.0.5"]
-                 [hiccup "1.0.5"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  ;; user must provide her own datomic
-                                  [com.datomic/datomic-free "0.9.4572"]]}}
-  :jvm-opts ["-Xmx1g"]
-  :eval-in-leiningen true)
+  :dependencies [[hiccup "1.0.5"]
+                 [jcf/dorothy "0.0.7-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.4572"]
+                                  [org.clojure/clojure "1.8.0"]]}}
+  :jvm-opts ["-Xmx1g"])
